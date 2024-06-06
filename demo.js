be967d2bb2ca93c24edd6f2f889548147bc2fb57
Jayashree Balineni <115000642+jayashreebalinani@users.noreply.github.com>
2024-06-06 08:15:29 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,7 +3,7 @@
 // //   "class":2,
 // //   "gender":"female"
 // // }
-// // //json-server --watch db.jso
+// // //json-server --watch 
 
 // // function fetch1(){
 // // fetch("http://localhost:3000/student/name")
